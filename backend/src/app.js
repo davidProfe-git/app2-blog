ce968ba2f332a94c70c9fc1f9e966416caee285e
paula_moreno <morenopaula806@gmail.com>
2025-11-28 08:20:00 -0500

    modificaciones

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,15 @@ const EXPRESS = require('express')
 const APP = EXPRESS()
 require('dotenv').config()
 const PORT = process.env.PORT
-const RecetaRoutes = require('./routes/RecetaRoutes')
+const RECETASROUTES = require('./routes/recetasRoutes')
+APP.use('/api/recetas',RECETASROUTES)
 
-APP.use(EXPRESS.json())
+APP.get('/',(request, response) =>{
+    response.json({mensaje : "Funciona la API"})
+})
 
-APP.use('/api/Receta', RecetaRoutes)
-
-APP.get('/',(request, response)=>{
-    response.json("mensaje : funciona la API")
-} )
 
 APP.listen(PORT, ()=>{
-    console.log(`El servidor esta corriendo en el pueerto ${PORT}`)
+    console.log(`el servidor esta corriendo en el puerto ${PORT}`)
+
 })
--- backend/src/routes/recetasRoutes.js
+++ backend/src/routes/recetasRoutes.js
@@ -1,6 +1,6 @@
 const EXPRESS = require('express')
 const ROUTER = EXPRESS.Router()
-const CONTROLLER = require('../controllers/RecetaControllers')
+const CONTROLLER = require('../controllers/recetasController');
 
 //mis rutas de la api
 ROUTER.get('/',CONTROLLER.obtenerRecetas)
