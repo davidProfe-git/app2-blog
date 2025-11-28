c1e5d9cf3acf7c2eae484469aa1a8e392adbae5f
paula_moreno <morenopaula806@gmail.com>
2025-11-28 11:24:14 -0500

    modificaciones

--- backend/src/controllers/recetasController.js
+++ backend/src/controllers/recetasController.js
@@ -1,21 +1,18 @@
 const MODEL = require('../models/recetasModel')
 
-class RecetasController {
-    // GET /api/recetas - obtiene el listado de recetas
-    static async obtenerRecetas(request, response) {
-        try {
-            const recetas = await MODEL.obtenerRecetas();
+class RecetasController{
+    //Get /api/recetas - obtiene le listado de recetas
+    static async obtenerRecetas(request, response){
+        
+            const recetas = await MODEL.obtenerRecetas()
             response.json({
                 success: true,
                 data: recetas
-            });
-        } catch (error) {
-            response.status(500).json({
-                success: false,
-                error: error.message
-            });
-        }
+            })
+
+        
     }
+
 }
 
-module.exports = RecetasController;
+module.exports = RecetasController
--- backend/src/models/recetasModel.js
+++ backend/src/models/recetasModel.js
@@ -1,26 +1,28 @@
-const DB = require('../config/database')
+const DB = require('../config/database');
 
-class RecetaModel{
+class RecetaModel {
 
-   //Read de nuestro CRUD
-   static async obtenerRecetas(){
-    try {
-        const [rows] = await DB.query('SELECT * FROM recetas')
-        return rows
-    } catch (error) {
-        throw error
+    // Obtener todas las recetas
+    static async obtenerRecetas() {
+        try {
+            const [rows] = await DB.query('SELECT * FROM recetas');
+            return rows;
+        } catch (error) {
+            throw error;
+        }
     }
 
-
-
-
-
-
-
-   } 
+    // Obtener receta por ID
+    static async obtenerPorId(id) {
+        try {
+            const [rows] = await DB.query('SELECT * FROM recetas WHERE id = ?', [id]);
+            return rows[0]; // devuelve solo la primera receta encontrada
+        } catch (error) {
+            throw error;
+        }
+    }
 
 }
 
-module.exports = RecetaModel
-
+module.exports = RecetaModel;
 
