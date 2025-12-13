bb0bde92f23b3fcec84b26f19044b9a0736b8698
alexander <jalexanderbef@gmail.com>
2025-12-12 21:03:49 -0500

    Resuelvo conflictos merge alexander

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const PORT = process.env.PORT
 const RECETASROUTES = require('./routes/recetasRoutes')
 APP.use(cors())
+const PORT = process.env.PORT
+const RECETASROUTES = require('./routes/recetasRoutes')
 APP.use('/api/recetas',RECETASROUTES)
 
 APP.get('/',(request, response) =>{
